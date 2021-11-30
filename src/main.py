4c5fcf974d9251ee393646ebfe6a450c780291b9
l1ght <2504082957@qq.com>
2021-11-30 18:25:49 +0800

    add selenium driver

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
+from typing import List
+
 import requests
 import json
 import re
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+
+from selenium.webdriver.chrome.options import Options
+_options = Options()
+_options.add_argument('--headless')
+
 
 class User:
 
@@ -13,23 +23,30 @@ class User:
         self.auth_org: bool = False
 
 
-def find_auth_fans(uid: int) -> int:
+def find_auth_fans(uid: int) -> List[int]:
     """
     :param uid: the uid of the source user
     :return: the authenticated uid list in the fans list
     """
     url = "https://space.bilibili.com/{}/fans/fans".format(uid)
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    try:
-        auth_id = soup.find("div", class_="zm-profile-card zm-profile-section-item zg-clear no-hovercard").find("a")["href"].split("/")[-1]
-    except:
-        auth_id = 0
-    return auth_id
+    wd = webdriver.Chrome(options=_options)
+
+    wd.get(url)
+    # print(wd.page_source)
+    soup = BeautifulSoup(wd.page_source, "html.parser")
+    print(soup.prettify())
+
+    _ret = []
+
+    return _ret
+
 
 uid_source: int = 208259  # 陈睿
 # uid_source: int = 546195  # 老番茄
 
 
 if __name__ == "__main__":
-    print("hello world".__class__)
+    i = find_auth_fans(uid_source)
+
+
+
