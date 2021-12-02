df7bf62547e5f63e3b4b83909401991787658704
l1ght <2504082957@qq.com>
2021-12-02 14:06:07 +0800

    fix the max page size to fit the api limit

--- src/main.py
+++ src/main.py
@@ -32,45 +32,48 @@ def find_auth_follows(uid: int) -> List[int]:
     :return: the authenticated uid list in the follow list
     """
     url = "https://space.bilibili.com/{}/fans/follow".format(uid)
-    wd = webdriver.Chrome(options=_options)
+    # wd = webdriver.Chrome(options=_options)
+    wd = webdriver.Chrome()
 
     wd.get(url)
+    time.sleep(0.5)
     # wd.find_element(by="class name", value="be-pager-total")
     total_pages = int(re.search('[0-9]+', wd.find_element(by="class name", value="be-pager-total").text).group())  # get the total pages
     print("total pages: {}".format(total_pages))
 
     _ret = []
 
-    for page_idx in range(total_pages):
+    for page_idx in range(min(total_pages, 5)):
         print("page: {}".format(page_idx))
         try:
             auth_users = wd.find_elements(by="xpath", value="//li[@class='list-item clearfix']/div[@class='content']/p[@class='auth-description']/../a[@class='title']/span")
             auth_users_href = wd.find_elements(by="xpath", value="//li[@class='list-item clearfix']/div[@class='content']/p[@class='auth-description']/../a[@class='title']")
             auth_users_id = [int(re.search('[0-9]+', auth_users_href[i].get_attribute("href")).group()) for i in range(len(auth_users_href))]
             _ret.extend([i for i in auth_users_id])
-            # print([i.text for i in auth_users])
+            print([i.text for i in auth_users])
             wd.find_element(by="class name", value="be-pager-next").click()  # click the next page
-            time.sleep(0.1)
+            time.sleep(1)
         except Exception as e:
             print("error in uid: {} and page: {}".format(uid, page_idx))
-            print(e)
+            # print(e)
             break
 
-    a=1
-
     # print(wd.find_element(by="class name", value="auth-description").text)
-
+    # wd.quit()
     # print(wd.page_source)
     soup = BeautifulSoup(wd.page_source, "html.parser")
     with open('1.html', 'w') as file_object:
         file_object.write(soup.prettify())
+    print("Found {} auth users from {}".format(len(_ret), uid))
 
 
     return _ret
 
 
-uid_source: int = 208259  # 陈睿
+# uid_source: int = 208259  # 陈睿
 # uid_source: int = 546195  # 老番茄
+uid_source: int = 946974  # 影视飓风
+# uid_source: int = 12590  # epcdiy
 
 
 if __name__ == "__main__":
